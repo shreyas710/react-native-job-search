1ac33bcbb7feda9e3c6a2df8f3a0d6da7bb8f22b
shreyas710 <shreyasnk5321@gmail.com>
2024-05-28 10:49:04 -0700

    welcome screen ui/ux done

--- app/index.js
+++ app/index.js
@@ -14,10 +14,38 @@ const Home = () => {
     const router = useRouter();
 
     return (
-        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
-            <Stack.Screen options={{
-                headerStyle: {backgroundColor: COLORS.lightWhite},
-            }}/>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => {
+                        return (
+                            <ScreenHeaderBtn
+                                iconUrl={icons.menu}
+                                dimension='60%'
+                            />
+                        );
+                    },
+                    headerRight: () => {
+                        return (
+                            <ScreenHeaderBtn
+                                iconUrl={images.profile}
+                                dimension='100%'
+                            />
+                        );
+                    },
+                    headerTitle: "",
+                }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{ flex: 1, padding: SIZES.medium }}>
+                    <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
         </SafeAreaView>
     );
 };
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,72 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    Image,
+    FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
 
-import styles from './welcome.style'
+import styles from "./welcome.style";
+
+import { icons, SIZES, COLORS } from "../../../constants";
 
 const Welcome = () => {
-  return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+    const router = useRouter();
+    const [activeJobType, setActiveJobType] = useState("Full-time");
 
-export default Welcome
+    const jobTypes = ["Full-time", "Part-time", "Contractor"];
+
+    return (
+        <View>
+            <View style={styles.container}>
+                <Text style={styles.userName}>Hello Shreyas</Text>
+                <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+            </View>
+
+            <View style={styles.searchContainer}>
+                <View style={styles.searchWrapper}>
+                    <TextInput
+                        style={styles.searchInput}
+                        value=''
+                        onChange={() => {}}
+                        placeholder='What are you looking for?r'
+                    />
+                </View>
+
+                <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+                    <Image
+                        source={icons.search}
+                        resizeMode='contain'
+                        style={styles.searchBtnImage}
+                    />
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.tabsContainer}>
+                <FlatList
+                    data={jobTypes}
+                    renderItem={({ item }) => {
+                        return (
+                            <TouchableOpacity
+                                style={styles.tab(activeJobType, item)}
+                                onPress={() => {
+                                    setActiveJobType(item);
+                                    router.push(`/search/${item}`);
+                                }}>
+                                <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+                            </TouchableOpacity>
+                        );
+                    }}
+                    keyExtractor={item => item}
+                    contentContainerStyle={{columnGap: SIZES.small}}
+                    horizontal
+                />
+            </View>
+        </View>
+    );
+};
+
+export default Welcome;
